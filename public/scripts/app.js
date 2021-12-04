37161dd210a49725f6686a7f3f269fc311345eb4
Gleison Sensui <gleison.nsilva@hotmail.com>
2021-12-04 16:53:11 -0300

    create calculation to total loan payment.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,13 +6,37 @@ const id_client = 10001;
 let nameClient = 'Fulano';
 let lastNane = 'da Silva';
 let ageClient = 38;
-let interestRate = 0.03;
-let amountInstallment = 36;
-let loanAmount = 10000.00;
+let loanValue = 150000;
+let interestRate = 0.10;
+let paymentTime = 6;
 let creditStatus = 'yes';
 
-const customerGreeting = `Good Morning, Mr. ${nameClient} ${lastNane}. Your age is ${ageClient}. You're welcome.`;
+let interesAmount = loanValue * interestRate * paymentTime;
 
+
+/* 
+    Formula Juros compostos: M = PV * (1 + i) ** n
+    M: Montante
+    PV: Valor presente
+    i: Taxa de juros (indice)
+    n: tempo de financiamento
+*/
+
+/**
+    Formula Juros Simples = M = PV + (PV * i * n)
+    M: Montante
+    PV: Valor presente
+    i: Taxa de juros (indice)
+    n: tempo de financiamento
+ */
+
+let totalToPay = loanValue + interesAmount;
+const customerGreeting = `
+Good Morning, Mr. ${nameClient} ${lastNane}. 
+Your age is ${ageClient}. You're welcome.
+Your total Loan Amount is ${totalToPay.toFixed(2)}`;
+
+console.log(customerGreeting);
 
 
 

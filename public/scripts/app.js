1ed872c999dc2176af3d44b04ff5aa00611cd69b
Gleison Sensui <gleison.nsilva@hotmail.com>
2021-12-03 22:05:06 -0300

    create feature of register client and your show in console.

---
+++ public/scripts/app.js
@@ -0,0 +1,31 @@
+/**
+ *  Create a Register client data.
+ */
+
+const id_client = 10001;
+let nameClient = 'Fulano';
+let lastNane = 'da Silva';
+let ageClient = 38;
+
+let exibir = `${id_client} - ${nameClient} ${lastNane}  - ${ageClient}`;
+
+console.log(exibir);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

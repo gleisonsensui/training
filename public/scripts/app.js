cca7df82aba73274584f5a42d742895535311a9c
Gleison Sensui <gleison.nsilva@hotmail.com>
2021-12-03 22:31:22 -0300

    create registration of financial credit client.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,8 +6,19 @@ const id_client = 10001;
 let nameClient = 'Fulano';
 let lastNane = 'da Silva';
 let ageClient = 38;
+let interestRate = 0.03;
+let amountInstallment = 36;
+let loanAmount = 10000.00;
+let creditStatus = 'yes';
 
-let exibir = `${id_client} - ${nameClient} ${lastNane}  - ${ageClient}`;
+let exibir = `
+${id_client}
+${nameClient} ${lastNane} 
+${ageClient} 
+${interestRate} 
+${amountInstallment} 
+${loanAmount.toFixed(2)} 
+${creditStatus}`;
 
 console.log(exibir);
 
